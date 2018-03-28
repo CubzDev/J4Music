ca9795d9dd8dff39cb5aee06919d51a05f39791b
CubzGames <37118764+CubzGames@users.noreply.github.com>
2018-03-28 17:35:13 +0200

    Update MusicBot.js

--- MusicBot.js
+++ MusicBot.js
@@ -1,6 +1,6 @@
 const { Client, Util } = require('discord.js');
 const PREFIX = '$'
-const GOOGLE_API_KEY = require(process.env.GOOGLE_API_KEY)
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 
